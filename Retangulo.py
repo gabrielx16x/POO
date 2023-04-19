68d25294d3bae31f7b9528e2bd60983ccf6f7ec6
Biel <115087477+gabrielx16x@users.noreply.github.com>
2023-04-19 09:25:42 -0300

    continuar

---
+++ Retangulo.py
@@ -0,0 +1,11 @@
+from Ponto import Ponto
+
+P1 = Ponto(20,30)
+P2 = Ponto(32,40)
+P3 = Ponto(13,22)
+P4 = Ponto(10,15)
+
+class Retangulo:
+    def __init__(self):
+        
+
---
+++ Triangulo.py
@@ -0,0 +1,22 @@
+from Ponto import Ponto
+
+P1 = Ponto(20,30)
+P2 = Ponto(32,40)
+P3 = Ponto(13,22)
+P4 = Ponto(10,15)
+class Triangulo:
+    def __init__(self,a,b,c):
+        self.A = a
+        self.B = b
+        self.C = c
+
+    def calc_lado(self, A, B):
+
+        p1,p2 = #type:ignore
+        p3,p4 = B.get_info()
+        return ((x2-x1)**2 + (y2 - y1)**2)**0.5 #type:ignore
+    
+
+Tri = Triangulo(P1,P2,P3)
+
+Tri.calc_lado(5,10)
