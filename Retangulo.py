ce661d03888132081b6c2aec78ec94a23438dfe2
Biel <115087477+gabrielx16x@users.noreply.github.com>
2023-04-21 20:57:06 -0300

    Update Retangulo.py

--- Retangulo.py
+++ Retangulo.py
@@ -1,11 +1,29 @@
 from Ponto import Ponto
 
-P1 = Ponto(20,30)
-P2 = Ponto(32,40)
-P3 = Ponto(13,22)
-P4 = Ponto(10,15)
-
+P1 = Ponto(-2, 0)
+P2 = Ponto(-2, 5)
+P3 = Ponto(4, 5)
+P4 = Ponto(4, 0)
 class Retangulo:
-    def __init__(self):
-        
+    def __init__(self, ponto1, ponto2, ponto3, ponto4):
+        self.a = ponto1
+        self.b = ponto2
+        self.c = ponto3
+        self.d = ponto4
 
+    def calc_perimetro(self):
+        l1 = Ponto.calc_dist_entre_pontos(self.a, self.b)#reutilização de código
+        l2 = Ponto.calc_dist_entre_pontos(self.b, self.c)
+        perimetro = 2 * (l1 + l2)#Cálculo que define o perímetro
+        return perimetro
+
+    def calc_area(self):
+        l1 = Ponto.calc_dist_entre_pontos(self.a, self.b)#reutilização de código
+        l2 = Ponto.calc_dist_entre_pontos(self.b, self.c)
+        area = l1 * l2 #Cálculo que define a área
+        return area
+
+Ret = Retangulo(P1,P2,P3,P4)
+
+print(Ret.calc_perimetro())
+print(Ret.calc_area())
